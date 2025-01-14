2f856dc1ea917401c799205d64bcbaa53c1ee79e
TheChiporpoise <thechiporpoise@gmail.com>
2025-01-13 21:06:25 -0700

    Prove 1 & 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lasstName = Console.ReadLine();
+
+        Console.WriteLine($"\nYour name is {firstName}, {firstName} {lasstName}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Text.Json.Nodes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        float grade = -1f;
+        while (100.0 < grade || grade < 0.0)
+        {
+            Console.Write("What is your grade out of 100? ");
+            grade = float.Parse(Console.ReadLine());
+        }
+
+        string letter = "";
+        string pass = "passed";
+        if (grade >= 90)
+        {
+            letter = "n A";
+        }
+        else if (grade >= 80)
+        {
+            letter = " B";
+        }
+        else if (grade >= 70)
+        {
+            letter = " C";
+        }
+        else if (grade >= 60)
+        {
+            letter = " D";
+            pass = "failed";
+        }
+        else
+        {
+            letter = "n F";
+            pass = "failed";
+        }
+        Console.WriteLine($"You got a{letter}, you {pass}!");
     }
 }
